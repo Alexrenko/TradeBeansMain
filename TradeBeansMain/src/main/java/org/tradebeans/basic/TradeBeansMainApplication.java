ca194393c674518f251a1b81fc2b6f9abffd2812
Александр Слюсаренко <aleksandrslyusarenko@MacBook-Air-Aleksandr.local>
2023-01-27 22:18:05 +0100

    created and compiled an empty spring web project

---
+++ TradeBeansMain/src/main/java/org/tradebeans/basic/TradeBeansMainApplication.java
@@ -0,0 +1,13 @@
+package org.tradebeans.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBeansMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeBeansMainApplication.class, args);
+    }
+
+}
---
+++ TradeBeansMain/src/test/java/org/tradebeans/basic/TradeBeansMainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tradebeans.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBeansMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
